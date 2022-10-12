b61868322571ae8ddce77094786dd683ef51f788
Hyerin Lim <70150896+limhyerin@users.noreply.github.com>
2022-10-12 22:16:27 +0900

    Add files via upload

---
+++ math_2.py
@@ -0,0 +1,8 @@
+# 원의 반지름을 입력받아 결과 도출 프로그램
+
+str_input = input("원의 반지름 입력")
+num_input = float(str_input)
+
+print("반지름:", num_input)
+print("둘레:", 2*3.14*num_input)
+print("넓이:", 3.14*num_input**2)

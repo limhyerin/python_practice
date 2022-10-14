a4f683afcc72de715b530b8593f090a4afb0400d
Hyerin Lim <70150896+limhyerin@users.noreply.github.com>
2022-10-14 12:18:38 +0900

    Add files via upload

---
+++ page229_4.py
@@ -0,0 +1,51 @@
+"""
+# 딕셔너리 안에 있는 리스트 같이 출력
+
+character = {
+    "name" : {"기사"},
+    "level" : 12,
+    "items" : {
+        "sword":"불꽃의 검",
+        "armor":"풀플레이트"
+    },
+    "skil":["베기","세게 베기", "아주 세게 베기"] 
+}
+
+for key in character:
+    if type(character[key]) is list:
+        for j in character[key]:
+            print(key,":", j)
+    elif type(character[key]) is dict: 
+        for z in character[key]:
+            print(z,":",character[key][z])
+    else:
+        print(key,":",character[key])
+
+
+
+"""
+
+
+
+#re
+#type(대상) is list / dict
+character = {
+    "name" : {"기사"},
+    "level" : 12,
+    "items" : {
+        "sword":"불꽃의 검",
+        "armor":"풀플레이트"
+    },
+    "skil":["베기","세게 베기", "아주 세게 베기"] 
+}
+
+for key in character:
+    if type(character[key]) is dict: # 딕셔너리인지 여부 확인
+        for i in character[key]: 
+            print(i, ":", character[key][i])
+    elif type(character[key]) is list: # 리스트인지 여부 확인
+        for j in character[key]:
+            print(key, ":", j)
+    else:
+        print(key, ":", character[key])
+    

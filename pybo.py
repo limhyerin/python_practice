fd5d1b57e07e9136db9b5b5f1c7d8bc86ad85946
Hyerin Lim <70150896+limhyerin@users.noreply.github.com>
2022-10-26 17:22:37 +0900

    Update pybo.py

--- pybo.py
+++ pybo.py
@@ -1,3 +1,7 @@
+![코드](https://user-images.githubusercontent.com/70150896/197973530-9be591d3-1a40-441c-89a9-c8a98a7297ad.png)
+![실행](https://user-images.githubusercontent.com/70150896/197973547-98002a3e-c95f-46da-a6bb-ad1c9ab97511.png)
+![결과](https://user-images.githubusercontent.com/70150896/197973562-7fc7579b-ec2e-4daf-be11-7dc1763e85e4.png)
+
 # 모듈 읽어 들임
 from flask import Flask
 from urllib import request

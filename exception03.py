90ee5e730151e6a869c68250d51baa04307ecc8d
Hyerin Lim <70150896+limhyerin@users.noreply.github.com>
2022-10-27 15:25:24 +0900

    Add files via upload

---
+++ exception03.py
@@ -0,0 +1,22 @@
+list_number = [52,273,32,72,100]
+
+# try except 구문으로 예외를 처리합니다
+try:
+    # 숫자를 입력받습니다
+    number_input = int(input("정수입력>>"))
+    # 리스트의 요소를 출력합니다
+    print("{}번째 요소: {}".format(number_input, list_number[number_input]))
+    예외.발생해주세요()
+except ValueError as exception:
+    print("함수를 입력해주세요")
+    print(type(exception), exception)
+
+except IndexError as exception:
+    print("리스트의 인덱스를 벗어났어요")
+    print(type(exception), exception)
+
+except NameError as exception:
+    print(type(exception), exception)
+
+except Exception as exception:
+    print(type(exception), exception)
